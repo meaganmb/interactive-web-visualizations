8df82d8f5445974d0b1398646b40808f0fb0ddee
Meagan Murray-Bruce <meaganmb_11@Meagans-MacBook-Pro.local>
2021-09-17 11:00:00 -0700

    can run json file

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-// Sort the data by OTU results descending
-let sortedByOTU = metadata.sort((a, b) => b.wfreq - a.wfreq);
+// d3.json("samples.json", function(data) {
+//     console.log(data.names);
+// });
 
-// Slice the first 10 objects for plotting
-slicedData = sortedByOTU.slice(0, 10);
+var otu = d3.json("samples.json").then(function(data){ console.log(data)});
 
-// Reverse the array to accommodate Plotly's defaults
-reversedData = slicedData.reverse();
 
-// Trace1 for the OTU Data
-let trace1 = {
-  x: reversedData.map(object => object.wfreq),
-  y: reversedData.map(object => object.id),
-  text: reversedData.map(object => object.id),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
+// var uk = Object.values(data.uk);
+// var canada = Object.values(data.canada);
 
-let traceData = [trace1];
+// Create an array of music provider labels
+// var labels = Object.keys(data.us);
 
-// Note that we use `traceData` here, not `data`
-Plotly.newPlot("plot", traceData);
+// // Sort the data by OTU results descending
+// let sortedByOTU = metadata.sort((a, b) => b.wfreq - a.wfreq);
+
+// // Slice the first 10 objects for plotting
+// slicedData = sortedByOTU.slice(0, 10);
+
+// // Reverse the array to accommodate Plotly's defaults
+// reversedData = slicedData.reverse();
+
+// // Trace1 for the OTU Data
+// let trace1 = {
+//   x: reversedData.map(object => object.wfreq),
+//   y: reversedData.map(object => object.id),
+//   text: reversedData.map(object => object.id),
+//   name: "OTU Data",
+//   type: "bar",
+//   orientation: "h"
+// };
+
+// let traceData = [trace1];
+
+// // Note that we use `traceData` here, not `data`
+// Plotly.newPlot("plot", traceData);
